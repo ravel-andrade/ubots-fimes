35fd0baa1f6efa8c5bf2d940e4733a6455a8933e
Ravel Andrade <ravel.gou@gmail.com>
2021-02-11 20:08:44 -0300

    first commit

---
+++ src/main/java/com/ravel/socialmovies/SocialmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.ravel.socialmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravel/socialmovies/SocialmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravel.socialmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
